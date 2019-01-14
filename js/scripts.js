7d75655bcc9e1acccd5ea2b82372e117e54886ff
Drumachine <nikita@Nikitas-MacBook-Pro.local>
2019-01-14 19:20:13 +0000

    Clipboard

--- js/scripts.js
+++ js/scripts.js
@@ -79,14 +79,8 @@ $("#export").on("click", startExport);
 $("#toString").on("click", toString);
 $("#toggleChecked").on("click", toggleChecked);
 
-$("#import").on("click", function(e) {
-  var clipboardData;
-  if (window.clipboardData && window.clipboardData.getData) { // IE
-      clipboardData = window.clipboardData.getData('Text');
-    } else {
-      clipboardData = (e.originalEvent || e).clipboardData.getData('text/plain');
-    }
-  startImport(clipboardData);
+$("#import").on("click", function() {
+  startImport(navigator.clipboard.readText());
 
 
 });
